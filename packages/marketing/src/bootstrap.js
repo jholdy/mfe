616f97ed06d86ac1806040dbc6609c14a9706138
Jholdy Damasceno <jholdy.damasceno@mobs2.com>
2021-01-25 22:36:03 -0300

    start commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+// Muont function to start up the app
+
+const mount = el => {
+  ReactDOM.render(<App />, el);
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
